57697fc4e7a508a1d1d791d78565a3b465bf3f98
pademanu <eruizp977@hotmail.com>
2023-10-26 18:02:22 -0600

    git commit -m 'script added'

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const yours = ['your mom', 'your dad', 'your pet', 'your sister', 'your brother', 'your aunt', 'your uncle'];
 const sexualities = ['Heterosexual', 'gay', 'lesbian', 'bisexual', 'pansexual', 'cisgender', 'asexual', 'queer'];
 
-const randomYours = arr => {
-    Math.floor(Math.random() * yours.length);
+const randomYours = arr1 => {
+    const pronoms = arr1;
+    const randomYours = Math.floor(Math.random() * arr1.length);
+    return pronoms[randomYours];
 };
 
-console.log(randomYours);
+const randomSexualities = arr2 => {
+    const genders = arr2;
+    const randomGenders = Math.floor(Math.random() * arr2.length);
+    return genders[randomGenders];
+};
+
+const createRandomMessage = () => {
+    const first = randomYours(yours);
+    const second = randomSexualities(sexualities);
+    const message = `${first} is ${second}`;
+    return message;
+};
+
+console.log(createRandomMessage());
