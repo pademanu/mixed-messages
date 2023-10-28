393b827921a43eb849ca990959ae77dc5c220214
pademanu <eruizp977@hotmail.com>
2023-10-28 17:12:34 -0600

    html and css added plus a few changes in the main.js syntax

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
+let phrase = document.getElementById('phrase');
+let button = document.getElementById('button');
+
+
 const yours = ['your mom', 'your dad', 'your pet', 'your sister', 'your brother', 'your aunt', 'your uncle'];
 const sexualities = ['Heterosexual', 'gay', 'lesbian', 'bisexual', 'pansexual', 'cisgender', 'asexual', 'queer'];
 
-const randomYours = arr1 => {
-    const pronoms = arr1;
-    const randomYours = Math.floor(Math.random() * arr1.length);
-    return pronoms[randomYours];
+function generateYours() {
+    let randomYours = Math.floor(Math.random() * yours.length);
+    return yours[randomYours];
 };
 
-const randomSexualities = arr2 => {
-    const genders = arr2;
-    const randomGenders = Math.floor(Math.random() * arr2.length);
-    return genders[randomGenders];
+function generateSexualities() {
+    let randomGenders = Math.floor(Math.random() * sexualities.length);
+    return sexualities[randomGenders];
 };
 
-const createRandomMessage = () => {
-    const first = randomYours(yours);
-    const second = randomSexualities(sexualities);
-    const message = `${first} is ${second}`;
-    return message;
+function createRandomMessage() {
+    let first = generateYours();
+    let second = generateSexualities();
+    let message = `${first} is ${second}`;
+    phrase.innerHTML = message;
 };
 
-console.log(createRandomMessage());
+button.addEventListener('click', createRandomMessage);
